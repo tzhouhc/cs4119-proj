6187abbf794f2f8a64b46749fd7fee9b5726d6a3
Ting Zhou <zhoutingneyc@gmail.com>
2025-04-18 03:55:07 -0400

    feat: prelim blockchain and tests

--- lib/blockchain.py
+++ lib/blockchain.py
@@ -41,7 +41,7 @@ class Block:
         """Mine for required difficulty and finalize block."""
         if self.done:
             return
-        while not self.valid():
+        while not self.is_valid():
             self.nonce += 1
             self.hash = self.get_hash()
         self.done = True
@@ -53,7 +53,7 @@ class Block:
         else:
             raise AttributeError(f"Can't modify read-only attribute {key}")
 
-    def valid(self) -> bool:
+    def is_valid(self) -> bool:
         """Hash matches required difficulty and regenerated hash."""
         if not self.hash.startswith(expected_hash_prefix()):
             return False
@@ -61,10 +61,60 @@ class Block:
 
 
 class BlockChain:
+
     def __init__(self):
-        self.chain = []
+        # always initialize chain with a null starter
+        self._chain: list[Block] = []
+
+    def __len__(self):
+        return len(self._chain)
+
+    def tail(self) -> Block | None:
+        if self._chain:
+            return self._chain[-1]
+        return None
 
     def append(self, block: Block) -> None:
         """Add a new block to the chain."""
-        # TODO: actually do checks
-        self.chain += [block]
+        if not block.is_valid():
+            raise ValueError("Block is invalid")
+        if not self._chain:
+            self._chain += [block]
+        else:
+            tail = self.tail()
+            assert tail is not None
+            if block.prev_hash != tail.hash:
+                raise ValueError("Block hash does not match chain")
+            self._chain += [block]
+
+    def is_valid(self) -> bool:
+        """validate full chain."""
+        for i in range(len(self) - 1):
+            prev = self._chain[i]
+            cur = self._chain[i + 1]
+            if cur.is_valid():
+                log.info(f"Chain entry {i} invalid.")
+                return False
+            if prev.hash != cur.prev_hash:
+                log.info(f"Chain entry {i} and {i+1} have mismatching hash.")
+                return False
+        return True
+
+    def get_block(self, i: int) -> Block | None:
+        if i >= len(self):
+            return None
+        return self._chain[i]
+
+    def get_block_by_hash(self, hash: str) -> Block | None:
+        for block in self._chain:
+            if block.hash.startswith(hash):
+                return block
+        return None
+
+    def grow(self, payload: bytes) -> None:
+        if not self._chain:
+            self._chain += [Block(payload, '')]
+        else:
+            tail = self.tail()
+            assert tail is not None
+            self._chain += [Block(payload, tail.hash)]
--- test/test_blockchain.py
+++ test/test_blockchain.py
@@ -5,6 +5,16 @@ from lib.utils import setup_logger
 
 log = setup_logger(2, name=__name__)
 
+def random_raw_block():
+    payload = random.randbytes(64)
+    prev_hash = str(random.randbytes(32))
+    return Block(payload, prev_hash)
+
+def random_block():
+    b = random_raw_block()
+    b.mine()
+    return b
+
 
 class TestBlock(unittest.TestCase):
 
@@ -44,7 +54,7 @@ class TestBlock(unittest.TestCase):
             b.mine()
             self.assertEqual(payload, b.payload)
             self.assertEqual(prev_hash, b.prev_hash)
-            self.assertTrue(b.valid())
+            self.assertTrue(b.is_valid())
 
 
 class TestBlockChain(unittest.TestCase):
@@ -55,13 +65,56 @@ class TestBlockChain(unittest.TestCase):
     def test_initializing(self):
         c = BlockChain()
         b = Block(b'', '')
+        b.mine()
         c.append(b)
 
-    def test_adding(self):
+    def test_adding_first_invalid_fail(self):
+        """The first block needs to be valid."""
         c = BlockChain()
-        for _ in range(20):
-            payload = random.randbytes(64)
-            prev_hash = str(random.randbytes(32))
-            b = Block(payload, prev_hash)
-            b.mine()
+        b = random_raw_block()
+        with self.assertRaises(ValueError):
             c.append(b)
+
+    def test_adding_first_no_prevhash_ok(self):
+        """The first block does not need to have a valid prev_hash."""
+        c = BlockChain()
+        b = random_block()
+        c.append(b)
+
+    def test_adding_subsequent_invalid_fail(self):
+        """
+        All subsequent blocks need to be valid AND have matching prev_hash.
+        """
+        c = BlockChain()
+        b = random_block()
+        c.append(b)
+        b2 = random_raw_block()
+        with self.assertRaises(ValueError):
+            c.append(b2)
+
+    def test_adding_subsequent_mismatching_fail(self):
+        """
+        All subsequent blocks need to be valid AND have matching prev_hash.
+        """
+        c = BlockChain()
+        b = random_block()
+        c.append(b)
+        b2 = random_block()
+        with self.assertRaises(ValueError):
+            c.append(b2)
+
+    def test_grow_first_ok(self):
+        c = BlockChain()
+        c.grow(b'hello')
+
+    def test_grow_second_ok(self):
+        c = BlockChain()
+        c.grow(b'hello')
+        c.grow(b'world')
+
+    def test_check_validity(self):
+        c = BlockChain()
+        for _ in range(10):
+            payload = random.randbytes(64)
+            c.grow(payload)
+        self.assertTrue(c.is_valid())
